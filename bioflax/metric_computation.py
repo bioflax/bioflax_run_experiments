changes that fix the slow runtime for metric computation

bioflax/metric_computation.py
@@ -5,8 +5,8 @@ import jax.tree_util as jax_tree
 from functools import partial
 
 
-@partial(jax.jit, static_argnums=(3))
-def adjust_grads(grads, grads_, angle, use_fixed_direction, n):
+@partial(jax.jit, static_argnums=(3, 5))
+def adjust_grads(grads, grads_, angle, use_fixed_direction, n, simulate):
     if use_fixed_direction:
         grads_extracted = n
     else:
@@ -31,21 +31,33 @@ def adjust_grads(grads, grads_, angle, use_fixed_direction, n):
     # p = g_ - alpha*g
     p = jax.tree_map((lambda a, b: a - alpha*b), grads_extracted, grads_)
 
+    
+
     kernel_p, _ = jax_tree.tree_flatten(
         flatten_matrices_in_tree(remove_keys(p, ['bias', 'B'])))
     bias_p, _ = jax_tree.tree_flatten(
         flatten_matrices_in_tree(remove_keys(p, ['kernel', 'B'])))
+    
+    jnp.allclose(0, jnp.sum(jnp.array(jax.tree_map((lambda a, b, c, d : a.dot(b) + c.dot(d)), kernel_p, kernel_, bias_p, bias))))
 
     # compute norm of norm_p and norm_g
     squared_norms_ = squared_norm(bias_p, kernel_p)
     squared_norms = squared_norm(bias_, kernel_)
+    squared_norms_fa = squared_norm(bias, kernel)
     norm_p = jnp.sqrt(jnp.sum(jnp.array(squared_norms_)))
     norm_g = jnp.sqrt(jnp.sum(jnp.array(squared_norms)))
+    norm_g_ = jnp.sqrt(jnp.sum(jnp.array(squared_norms_fa)))
 
     # final_grad = norm_g * (cos(angle)/norm_g * g + sin(angle)/norm_p * p) = cos(angle)*g + norm_g * sin(angle)/norm_p * p
-    final_grad = jax.tree_map((lambda a, b: jnp.cos(jnp.radians(
-        angle))*a + norm_g * jnp.sin(jnp.radians(angle))/norm_p * b), grads_, p)
-    return update_kernels_and_biases(grads, final_grad)
+
+    if simulate: 
+        #this is an effort to mimic FA via true gradient offsets and sceling so in this case I want the norm
+        final_grad = jax.tree_map((lambda a, b: norm_g_ * (jnp.cos(jnp.radians(
+            angle))/norm_g*a + jnp.sin(jnp.radians(angle))/norm_p * b)), grads_, p) 
+    else: 
+        final_grad = jax.tree_map((lambda a, b: jnp.cos(jnp.radians(
+            angle))*a + norm_g * jnp.sin(jnp.radians(angle))/norm_p * b), grads_, p)
+    return update_kernels_and_biases(grads, final_grad), norm_p
 
 
 def update_kernels_and_biases(dict1, dict2):
@@ -67,7 +79,7 @@ def update_kernels_and_biases(dict1, dict2):
 
     return dict1
 
-
+@partial(jax.jit, static_argnums = (3,4,5))
 def compute_metrics(state, grads_,  grads, mode, lam, use_bias):
     """
     Compute alignment metrics of current epoch for given state of the model and current gradients. Metrics computed are:
@@ -105,7 +117,7 @@ def compute_metrics(state, grads_,  grads, mode, lam, use_bias):
         flatten_matrices_in_tree(remove_keys(grads_, ['bias', 'B'])))
     kernel, _ = jax_tree.tree_flatten(
         flatten_matrices_in_tree(remove_keys(grads, ['bias', 'B'])))
-
+    
     bias_al_per_layer = compute_bias_grad_al_layerwise(bias_, bias)
     wandb_grad_al_per_layer = compute_wandb_grad_al_layerwise(
         bias_, bias, kernel_, kernel, use_bias)
@@ -113,7 +125,9 @@ def compute_metrics(state, grads_,  grads, mode, lam, use_bias):
         bias_, bias, kernel_, kernel, use_bias)
     rel_norm_grads = compute_rel_norm(bias_, bias, kernel_, kernel, use_bias)
     return bias_al_per_layer, wandb_grad_al_per_layer, wandb_grad_al_total, weight_al_per_layer, rel_norm_grads, norm_, norm
-
+    """
+    return 0,0,0,0,0,0,0
+    """
 
 def summarize_metrics_epoch(bias_als_per_layer, wandb_grad_als_per_layer, wandb_grad_als_total, weight_als_per_layer, rel_norms_grads, norms_, norms, mode):
     """
@@ -148,7 +162,7 @@ def summarize_metrics_epoch(bias_als_per_layer, wandb_grad_als_per_layer, wandb_
     return avg_bias_al_per_layer, avg_wandb_grad_al_per_layer, avg_wandb_grad_al_total, avg_weight_al_per_layer, avg_rel_norm_grads, avg_norm_, avg_norm
 
 
-# @jax.jit
+@jax.jit
 def compute_interpolate_weight_alignment(params, lam):
     """
     Computes the alignment of feedforward and feedback matrices per layer.
@@ -171,7 +185,7 @@ def compute_interpolate_weight_alignment(params, lam):
     return layerwise_alignments
 
 
-# @jax.jit
+@jax.jit
 def compute_weight_alignment(params):
     """
     Computes the alignment of feedforward and feedback matrices per layer.
@@ -193,7 +207,7 @@ def compute_weight_alignment(params):
     return layerwise_alignments
 
 
-# @jax.jit
+@jax.jit
 def compute_bias_grad_al_layerwise(bias_, bias):
     """
     Computes the alignment of bias gradients per layer: cos(theta) = (bias_.dot(bias))/(||bias_||*||bias||)
@@ -210,7 +224,7 @@ def compute_bias_grad_al_layerwise(bias_, bias):
     return layerwise_alignments
 
 
-# @jax.jit
+@partial(jax.jit, static_argnums=(4))
 def compute_wandb_grad_al_layerwise(bias_, bias, kernel_, kernel, use_bias):
     """
     Computes the alignment of weight & bias gradients per layer: 
@@ -239,7 +253,7 @@ def compute_wandb_grad_al_layerwise(bias_, bias, kernel_, kernel, use_bias):
     return layerwise_alignments
 
 
-# @jax.jit
+@partial(jax.jit, static_argnums=(4))
 def compute_wandb_al_total(bias_, bias, kernel_, kernel, use_bias):
     """
     Computes the alignment of total gradients per layer:
@@ -278,7 +292,7 @@ def compute_wandb_al_total(bias_, bias, kernel_, kernel, use_bias):
     return res, norm_, norm
 
 
-# @jax.jit
+@partial(jax.jit, static_argnums=(4))
 def compute_rel_norm(bias_, bias, kernel_, kernel, use_bias):
     """
     Computes the relative norm of gradient of current model compared to gradient of comparison model:

bioflax/train.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 from typing import Any
 from .metric_computation import compute_metrics
 from .model import BatchBioNeuralNetwork, BatchTeacher, BioNeuralNetwork
-from .train_helpers import get_loss, reorganize_dict, random_initialize_grad, prep_batch, create_train_state, train_epoch, train_step, validate, plot_sample, select_initializer
+from .train_helpers import alignment_step, get_loss, reorganize_dict, random_initialize_grad, prep_batch, create_train_state, train_epoch, train_step, validate, plot_sample, select_initializer
 from .dataloading import create_dataset
 
 
@@ -118,26 +118,27 @@ def train(args):
         key, num=8)
 
     # Create dataset
-    (
-        trainloader,
-        valloader,
-        testloader,
-        output_features,
-        seq_len,
-        in_dim,
-        train_size
-    ) = create_dataset(
-        seed=key_dataset[0].item(),
-        batch_size=batch_size,
-        dataset=dataset,
-        val_split=val_split,
-        input_dim=in_dim,
-        output_dim=output_features,
-        L=seq_len,
-        train_set_size=train_set_size,
-        test_set_size=test_set_size,
-        teacher_act=teacher_act,
-    )
+    if test_set_size > 0 or train_set_size > 0:
+        (
+            trainloader,
+            valloader,
+            testloader,
+            output_features,
+            seq_len,
+            in_dim,
+            train_size
+        ) = create_dataset(
+            seed=key_dataset[0].item(),
+            batch_size=batch_size,
+            dataset=dataset,
+            val_split=val_split,
+            input_dim=in_dim,
+            output_dim=output_features,
+            L=seq_len,
+            train_set_size=train_set_size,
+            test_set_size=test_set_size,
+            teacher_act=teacher_act,
+        )
     print(f"[*] Starting training on '{dataset}'...")
 
     # Learning rate scheduler
@@ -216,7 +217,7 @@ def train(args):
         optimizer=optimizer
     )
 
-    _ = create_train_state(
+    state_bp = create_train_state(
         model=bp_model,
         rng=key_model_bp,
         lr=lr,  # scheduler, relevant change at the moment
@@ -241,7 +242,7 @@ def train(args):
     # x = nn.initializers.uniform(scale=2)(
     #    key, shape=(batch_size, d_input, L)) - 1.
     labels = teacher_model.apply({'params': teacher_params}, inputs)
-    labels -= jnp.mean(labels, axis=0)
+    # das mal weglassen sonst unmöglich die teacher weights zu recovern labels -= jnp.mean(labels, axis=0)
 
     def loss_comp(params):
         logits = bp_model.apply({"params": params}, inputs)
@@ -252,8 +253,6 @@ def train(args):
     )
 
     n = random_initialize_grad(grads_, key_rand)
-    print(n)
-    print(grads_)
 
     # Training loop over epochs
     conv_rate = 0
@@ -273,23 +272,15 @@ def train(args):
             # x = nn.initializers.uniform(scale=2)(
             #    key, shape=(batch_size, d_input, L)) - 1.
             labels = teacher_model.apply({'params': teacher_params}, inputs)
-            labels -= jnp.mean(labels, axis=0)
+            # das mal weglassen sonst unmöglich teacher weights zu recovern labels -= jnp.mean(labels, axis=0)
             # inputs, labels = prep_batch(batch)
 
             if compute_alignments:
-
-                def loss_comp(params):
-                    logits = bp_model.apply({"params": params}, inputs)
-                    loss = get_loss(loss_fn, logits, labels)
-                    return loss
-
                 if mode != "bp":
-                    _, grads_ = jax.value_and_grad(loss_comp)(
-                        reorganize_dict({"params": state.params})["params"]
-                    )
+                    grads_ = alignment_step(state, state_bp, inputs, labels, loss_fn)
                 else:
-                    _, grads_ = jax.value_and_grad(loss_comp)(state.params)
-            state, loss, grads = train_step(
+                    grads_ = grads
+            state, loss, grads, norm_p = train_step(
                 state, inputs, labels, loss_fn, grads_, angle, use_fixed_direction, n)
             if compute_alignments:
                 (
@@ -301,7 +292,6 @@ def train(args):
                     norm_,
                     norm
                 ) = jited_compute_metrics(state, grads_, grads, mode, lam, use_bias)
-
                 if (i > 0):
                     conv_rate = loss/prev_loss
                 prev_loss = loss
@@ -315,8 +305,8 @@ def train(args):
                 "Training loss epoch": loss,
                 # "lr" : lr_
             }
-
             if compute_alignments:
+                metrics["norm_p"] = norm_p
                 metrics["lambda"] = lam
                 metrics["Relative norms gradients"] = rel_norm_grads
                 metrics["Gradient alignment"] = wandb_grad_al_total
@@ -338,7 +328,7 @@ def train(args):
                         metrics[f"Alignment layer {i}"] = al
             if use_wandb:
                 wandb.log(metrics)
-
+        """
         if valloader is not None:
             print(f"[*] Running Epoch {epoch + 1} Validation...")
             val_loss, val_acc = validate(
@@ -418,6 +408,7 @@ def train(args):
             if task == "classification":
                 wandb.run.summary["Best test accuracy"] = best_test_acc
                 wandb.run.summary["Best val accuracy"] = best_acc
+        """
 
     # print(state)
 

bioflax/train_helpers.py
@@ -12,7 +12,7 @@ from jax.nn import one_hot
 from tqdm import tqdm
 from flax.training import train_state
 from functools import partial
-from .metric_computation import compute_metrics, summarize_metrics_epoch, reorganize_dict, adjust_grads
+from .metric_computation import compute_metrics, flatten_matrices_in_tree, remove_keys, summarize_metrics_epoch, reorganize_dict, adjust_grads, compute_wandb_al_total
 
 
 def create_train_state(model, rng, lr, momentum, weight_decay, in_dim, batch_size, seq_len, optimizer):
@@ -197,13 +197,40 @@ def train_step(state, inputs, labels, loss_function, grads_, angle, use_fixed_di
         return loss
 
     loss, grads = jax.value_and_grad(loss_fn)(state.params)
-    if angle != -1:
-        grads = adjust_grads(grads, grads_, angle, use_fixed_direction, n)
+    if angle == -1:
+        norm_p = 0
+        state = state.apply_gradients(grads=grads)
+    elif angle == -2: 
+        bias_, _ = jax_tree.tree_flatten(
+                flatten_matrices_in_tree(remove_keys(grads_, ['kernel', 'B'])))
+        bias, _ = jax_tree.tree_flatten(
+            flatten_matrices_in_tree(remove_keys(grads, ['kernel', 'B'])))
+        kernel_, _ = jax_tree.tree_flatten(
+            flatten_matrices_in_tree(remove_keys(grads_, ['bias', 'B'])))
+        kernel, _ = jax_tree.tree_flatten(
+            flatten_matrices_in_tree(remove_keys(grads, ['bias', 'B'])))
+        al, _, _ = compute_wandb_al_total(
+            bias_, bias, kernel_, kernel, True)
+        angle = jnp.arccos(al) * 180/jnp.pi
+        grads, norm_p = adjust_grads(grads, grads_, angle, use_fixed_direction, n, False)
         state = state.apply_gradients(grads=grads)
     else:
+        grads, norm_p = adjust_grads(grads, grads_, angle, use_fixed_direction, n, False)
         state = state.apply_gradients(grads=grads)
-    return state, loss, grads
+    return state, loss, grads, norm_p
 
+@partial(jax.jit, static_argnums=(4))
+def alignment_step(state, state_bp, inputs, labels, loss_function):
+
+    def loss_comp(params):
+                    logits = state_bp.apply_fn({"params": params}, inputs)
+                    loss = get_loss(loss_function, logits, labels)
+                    return loss
+    
+    _, grads_ = jax.value_and_grad(loss_comp)(
+                        reorganize_dict({"params": state.params})["params"]
+                    )
+    return grads_
 
 def get_loss(loss_function, logits, labels):
     """