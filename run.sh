3c349ff8d8f623950c88c326301715d09d55040d
yschimpf <yaschimpf@gmail.com>
2024-03-14 20:04:09 +0100

    start a run

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-for DEVICE_NUM in 3
+for DEVICE_NUM in 2
 do
-  CUDA_VISIBLE_DEVICES=$DEVICE_NUM  wandb agent bioflax/bioflax_run_experiments/g9iu7ryy &
+  CUDA_VISIBLE_DEVICES=$DEVICE_NUM  wandb agent bioflax/bioflax_run_experiments/1eync3wg &
   sleep 3
 done
 
