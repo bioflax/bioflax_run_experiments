9dc13ebe82e22074fde3a9aea2652d29bbc045ae
yschimpf <yaschimpf@gmail.com>
2024-03-22 20:07:20 +0100

    non-periodic resetting

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-for DEVICE_NUM in 0 1
+for DEVICE_NUM in 0 1 2
 do
-  CUDA_VISIBLE_DEVICES=$DEVICE_NUM wandb agent bioflax/bioflax_run_experiments/3jfhzhoe &
+  CUDA_VISIBLE_DEVICES=$DEVICE_NUM wandb agent bioflax/bioflax_run_experiments/0x73yhvu &
   sleep 3
 done
 
